33f99a6fde2fc1440e625c9c1b403860c07d3332
egad13 <egad.coding@gmail.com>
2023-09-21 22:09:49 -0400

    Allow autoequipping 0-notch charms on full loadouts
    Also fixes a bug where rebuilding the charm loadout on notch pickups was considering the old notch total, not the new one.

--- UseWhatYouGet/IC/CharmAutoEquipModule.cs
+++ UseWhatYouGet/IC/CharmAutoEquipModule.cs
@@ -1,43 +1,50 @@
 ﻿using ItemChanger.Modules;
 using Modding;
 using Osmi.Game;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using UseWhatYouGet.Rando;
+using UseWhatYouGet.Util;
 
 namespace UseWhatYouGet.IC {
 	public class CharmAutoEquipModule : Module {
 
+		private static PlayerData PD => PlayerData.instance;
 		private static UwygRandoSettings.CharmSettings Settings => UseWhatYouGetMod.GS.RandoSettings.Charms;
 		private static Settings.LocalSettings SaveData => UseWhatYouGetMod.LS;
 
 		public override void Initialize() {
 			ModHooks.SetPlayerBoolHook += CharmGiven;
 			if (Settings.KeepFull) {
-				ModHooks.SetPlayerBoolHook += NotchGiven;
+				On.PlayerData.IncrementInt += NotchIncrementedHook;
+				On.PlayerData.DecrementInt += NotchDecrementedHook;
+				On.PlayerData.SetInt += NotchSetHook;
 			}
 		}
 
 		public override void Unload() {
 			ModHooks.SetPlayerBoolHook -= CharmGiven;
-			ModHooks.SetPlayerBoolHook -= NotchGiven;
+			On.PlayerData.IncrementInt -= NotchIncrementedHook;
+			On.PlayerData.DecrementInt -= NotchDecrementedHook;
+			On.PlayerData.SetInt -= NotchSetHook;
 		}
 
 		// HOOKS
 
 		private static bool CharmGiven(string boolName, bool value) {
-			if (!boolName.StartsWith("gotCharm") || !value) { return value; }
-
-			var pd = PlayerData.instance;
+			if (!boolName.StartsWith("gotCharm") || !value) return value;
 			int charmNum = int.Parse(boolName.Split('_')[1]);
 
 			SaveData.CharmHistory.Add(charmNum);
 
-			List<int> currentCharms = pd.GetVariable<List<int>>("equippedCharms");
+			List<int> currentCharms = PD.GetVariable<List<int>>(nameof(PD.equippedCharms));
 
-			int slotsMax = pd.GetInt("charmSlots"),
-				slotsUsed = pd.GetInt("charmSlotsFilled"),
-				cost = Settings.AllowOvercharm ? 1 : CharmUtil.GetCharmCost(charmNum);
+			int slotsMax = PD.GetInt(nameof(PD.charmSlots)),
+				slotsUsed = PD.GetInt(nameof(PD.charmSlotsFilled)),
+				cost =
+					Settings.AllowOvercharm ? Math.Min(1, CharmUtil.GetCharmCost(charmNum))
+					: CharmUtil.GetCharmCost(charmNum);
 
 			// Unequip oldest charm until there's room for a new one
 			while ((slotsMax - slotsUsed) < cost && currentCharms.Count > 0) {
@@ -47,45 +54,40 @@ namespace UseWhatYouGet.IC {
 			}
 
 			// Equip new charm (if possible)
-			if (cost <= slotsMax) {
-				CharmUtil.EquipCharm(charmNum);
-			}
-
+			if (cost <= slotsMax) CharmUtilEx.EquipCharm(charmNum);
 			CharmUtil.UpdateCharm();
 			return value;
 		}
 
-		private static bool NotchGiven(string boolName, bool value) {
-			if (!boolName.StartsWith("notch") && !boolName.StartsWith("salubraNotch")) { return value; }
-			var pd = PlayerData.instance;
+		private static void NotchIncrementedHook(On.PlayerData.orig_IncrementInt orig, PlayerData pd, string intName) {
+			orig(pd, intName);
+			if (intName == nameof(pd.charmSlots)) ReEquipCharms();
+		}
+		private static void NotchDecrementedHook(On.PlayerData.orig_DecrementInt orig, PlayerData pd, string intName) {
+			orig(pd, intName);
+			if (intName == nameof(pd.charmSlots)) ReEquipCharms();
+		}
+		private static void NotchSetHook(On.PlayerData.orig_SetInt orig, PlayerData pd, string intName, int value) {
+			orig(pd, intName, value);
+			if (intName == nameof(pd.charmSlots)) ReEquipCharms();
+		}
 
-			// Rebuild the equipped charms list from the charm history, as full as possible
-			int[] reversedHistory = SaveData.CharmHistory.Skip(0).Reverse().ToArray();
-			int slotsMax = pd.GetInt("charmSlots"),
-				cost = 0,
-				index = 0;
-
-			while (cost < slotsMax && index < reversedHistory.Length) {
-				cost += CharmUtil.GetCharmCost(reversedHistory[index]);
-				index++;
-			}
-			if (Settings.AllowOvercharm && index < reversedHistory.Length && slotsMax - cost >= 1) {
-				index++;
-			}
-
-			// Requip all charms IF NECESSARY.
-			List<int> newCharmList = reversedHistory.Take(index).Reverse().ToList();
-			List<int> currentCharms = pd.GetVariable<List<int>>("equippedCharms");
-
-			if (currentCharms.Equals(newCharmList)) { return value; }
+		private static void ReEquipCharms() {
+			// Rebuild the equipped charms list from the charm history into a full loadout.
+			// This makes ordering of the charms in the UI follow the pick up history.
+			var history = SaveData.CharmHistory.AsEnumerable().Reverse();
+			bool succeeded = true;
 
 			CharmUtil.UnequipAllCharms();
-			CharmUtil.EquipCharms(newCharmList.ToArray());
+			foreach(int charm in history) {
+				if (!succeeded || !CharmUtilEx.CanEquipCharm(charm)
+					|| (!Settings.AllowOvercharm && CharmUtilEx.WouldOvercharm(charm))) {
+					break;
+				}
+				succeeded = CharmUtilEx.EquipCharm(charm);
+			}
+			PD.GetVariable<List<int>>(nameof(PD.equippedCharms)).Reverse();
 			CharmUtil.UpdateCharm();
-			return value;
-
-			// It's done like this to preserve the ordering of the charms in the UI,
-			// which makes it easier to understand what's happening.
 		}
 
 	}
---
+++ UseWhatYouGet/Util/CharmUtilEx.cs
@@ -0,0 +1,58 @@
+﻿using Osmi.Game;
+
+namespace UseWhatYouGet.Util {
+	/// <summary>
+	/// "Extensions" on <see cref="CharmUtil"/>'s functionality. Primarily for making
+	/// equipping 0-notch charms possible even when all slots are filled.
+	/// </summary>
+	internal static class CharmUtilEx {
+
+		private static PlayerData PD => PlayerData.instance;
+		private static int Cost(int charm) => CharmUtil.GetCharmCost(charm);
+		private static int CharmSlots => PD.GetInt(nameof(PD.charmSlots));
+		private static int CharmSlotsFilled => PD.GetInt(nameof(PD.charmSlotsFilled));
+
+		/// <summary>
+		/// Checks if a charm can be equipped. Either it's a 0-notch charm and the player
+		/// is not overcharmed, OR it's any other cost and there's at least 1 notch of space.
+		/// </summary>
+		/// <param name="charm">Charm ID</param>
+		public static bool CanEquipCharm(int charm)
+			=> !CharmUtil.EquippedCharm(charm)
+				&& (
+					(Cost(charm) >= 1 && CharmSlotsFilled < CharmSlots)
+					|| (Cost(charm) == 0 && CharmSlotsFilled <= CharmSlots)
+				);
+
+		/// <summary>
+		/// Checks if a charm would cause overcharming if equipped at this time.
+		/// </summary>
+		/// <param name="charm">Charm ID</param>
+		public static bool WouldOvercharm(int charm)
+			=> CharmSlotsFilled + Cost(charm) > CharmSlots;
+
+		/// <summary>
+		/// Equip a charm if either: it's a 0-notch charm and the player is not
+		/// overcharmed, OR it's any other cost and there's at least 1 notch of space.
+		/// </summary>
+		/// <param name="charm">Charm ID</param>
+		/// <returns>Whether the operation is successful or not</returns>
+		/// <remarks>The caller should call <see cref="CharmUtil.UpdateCharm"/> after changes to charms</remarks>
+		public static bool EquipCharm(int charm) {
+			if (!CanEquipCharm(charm)) return false;
+
+			PD.SetInt(nameof(PD.charmSlotsFilled), CharmSlotsFilled + Cost(charm));
+
+			if (CharmSlotsFilled > CharmSlots) {
+				PD.SetBool(nameof(PD.canOvercharm), true);
+				PD.SetBool(nameof(PD.overcharmed), true);
+			}
+
+			PD.SetBool($"equippedCharm_{charm}", true);
+			PD.EquipCharm(charm);
+
+			return true;
+		}
+
+	}
+}
